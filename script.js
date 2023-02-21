1b34d798cba9d8ef6b073f053b4800426df69fee
SMIRITI MISHRA <113242412+smiritimis2@users.noreply.github.com>
2023-02-21 20:29:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
-/*const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
-
-const getSum = () => {
-//Add your code here
-  
-};*/
-
-getSumBtn.addEventListener("click", getSum);
-
 const getSumBtn = document.createElement("button");
-
 getSumBtn.append("Get Total Price");
-
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-
     //Add your code here
-
     const prices = document.querySelectorAll(".price");
-
     let sum = 0;
-
     prices.forEach(price => sum += parseFloat(price.innerText));
-
     const totalRow = document.createElement("tr");
-
     const totalCell = document.createElement("td");
-
     totalCell.colSpan = "2";
-
     totalCell.innerText = `Total Price: $${sum.toFixed(2)}`;
-
     totalRow.appendChild(totalCell);
-
     totalCell.setAttribute("id", 'ans')
-
     document.querySelector("table").appendChild(totalRow);
-
 };
 
 getSumBtn.addEventListener("click", getSum);
-
